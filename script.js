d9705d8c1fe21e6156860207265d63e2ebaa0d20
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2025-07-29 09:29:26 -0300

    asdfasdfasdf

--- script.js
+++ script.js
@@ -36,3 +36,11 @@ const perguntas = [
     ],
   },
   ];
+
+  let atual = 0;
+  let perguntaAtual;
+
+  function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+  }
